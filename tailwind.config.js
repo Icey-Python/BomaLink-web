685857e014f1d164ef7723b6e03a6b18e97374fe
Kinotijoan <kinotijoan@gmail.com>
2024-05-19 19:37:48 +0300

    Added montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
       mogra: ["mogra", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
   },
   plugins: [],
