8b35f25d01175323047bfd9fadd250583018d14d
Kinotijoan <kinotijoan@gmail.com>
2024-05-23 13:25:57 +0300

    Refactor: Changed aboutUs page to ourservices section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HomeSection from "@/components/Home";
+import OurServices from "@/components/OurServices";
 
 export default function Home() {
   return (
       <main>
         <HomeSection />
+        <OurServices tagId="service"/>
       </main>
   );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -3,24 +3,32 @@ import React, { ReactNode } from 'react';
 interface Props{
   iconPath: string,
   title: string,
+  onClick: (id: string) => void,
   children?: ReactNode,
+  cardId : string
 }
-const Card = ({iconPath,title,children}:Props) => {
+const Card = ({onClick,cardId, iconPath,title,children}:Props) => {
   return (
-    <div className="rounded-xl p-5 mx-2">
-        <Image className="mx-auto" src={`/icons/${iconPath}`} alt='Icon' width={120} height={106} style={{
-        width: 'auto', // Make the image width responsive
-        height: '120px', // Maintain aspect ratio by setting height to auto
-      }}/>
-        <div className="w-full h-2 flex justify-between">
-        <div className="w-1/3 h-1 bg-black rounded-xl"></div>
-        <div className="w-1/3 h-1 bg-black rounded-xl"></div>
-        </div>
-        <p className="font-semibold mt-2 text-green-700 text-xl">{title}</p>
-        <p className='font-medium'>
-        {children}
-        </p>
-        </div> 
+    <div className="transition-all duration-700 delay-50 cursor-pointer ease-out bg-transparent  p-5 mx-2 bg-white/50 text-black shadow-2xl z-40 hover:bg-[#238636] hover:bg-opacity-70 group max-w-xl hover:flex " onClick={() => onClick(cardId)} id={cardId}>
+      <div className="group-hover:hidden">
+          <Image className="mx-auto group-hover:scale-75  group-hover:z-0" src={`/icons/${iconPath}`} alt='Icon' width={120} height={106} style={{
+            width: 'auto', // Make the image width responsive
+            height: '120px', // Maintain aspect ratio by setting height to auto
+          }}/>
+      </div>
+      <div className="">
+          <div className="w-full h-2 flex justify-between group-hover:hidden">
+            <div className="w-1/3 h-1 bg-black rounded-xl "></div>
+            <div className="w-1/3 h-1 bg-black rounded-xl"></div>
+          </div>
+          <div className="h-32 space-y-2">
+            <p className="font-semibold mt-6 -[-20%] text-4xl text-center group-hover:text-2xl group-hover:text-left transition ease-in delay-50">{title}</p>
+            <p className='font-medium transition ease-out delay-50 invisible group-hover:visible'>
+            {children}
+            </p>
+          </div>
+      </div>
+      </div> 
 
   )
 }
