20ff68475291996f1e322bc849ed2b7c56d3300a
Kinotijoan <kinotijoan@gmail.com>
2024-05-23 12:10:13 +0300

    merged landing and services page

---
+++ components/appView/slide3.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from "next/image";
+
+const Slide3 = () => {
+  return (
+    <div className={'flex flex-col items-center justify-center h-screen font-poppins'}>
+          
+       <Image src="/app-view-images/track-animals-location.svg" alt="tracking animals" width={300} height={180}/>
+          
+    </div>
+  );
+};
+
+export default Slide3;
+
---
+++ components/appView/slide4.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from "next/image";
+
+const Slide4 = () => {
+  return (
+
+    <div className={'flex flex-col items-center justify-center h-screen font-poppins'}>
+          
+       <Image src="/app-view-images/Marketing platform.svg" alt="marketing platform" width={300} height={180}/>
+          
+    </div>
+  );
+};
+
+export default Slide4;
