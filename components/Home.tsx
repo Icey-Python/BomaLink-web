ee46cc9e95db2de5a2c29e5edef9d64a696cff78
Icey-Python <ndungusamkelly5@gmail.com>
2024-05-27 12:28:38 +0300

    Refactored landing page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import ParticlesComponent from "./ParticlesComponent"
+import {ArrowDownToLine,Info} from "lucide-react"
 interface Props{
     tagId : string
     
@@ -13,15 +14,17 @@ const HomeSection = ({tagId}:Props) => {
                     <div className="flex justify-between p-10 pt-20">
                         <div className="pt-20  space-y-6">
                             <p className="text-[190px] font-mogra text-white drop-shadow-md leading-none">Agrimeet</p>
-                            <div className="space-y-10">
+                            <div className="space-y-8">
                                 <p className="text-4xl text-[#D6F9C6] font-monteserrat drop-shadow-md leading-none font-extralight">Maamuzi sahihi, ushirikiano wa kudumu</p>
                                 <p className="text-white text-2xl font-poppins">A mobile app aimed at fostering peaceful coexistence <br/> between Kenyan pastoralists and agriculturalists <br/> by creating a digital platform that addresses resource conflict.</p>
-                                <button  className="text-white text-xl font-poppins  bg-[#021500] bg-opacity-60 p-4 ml-0 m-4 border-2 border-[#224103] rounded-full w-[200px] h-[65px] hover:text-[#47F969] hover:border-[#47F969]">Get App</button>
-                                <button  className="text-white text-xl font-poppins  bg-transparent p-4 ml-0 m-4 border-2 border-[#224103] rounded-full w-[200px] h-[65px] hover:text-[#47F969] hover:border-[#47F969]">Learn More</button>
+                                <div className="flex">
+                                <button  className="text-white text-xl font-poppins  bg-[#021500] bg-opacity-60 p-4 ml-0 mx-4 border-2 border-[#224103] rounded-full w-[200px] h-[65px] transition duration-500 hover:text-[#47F969] flex justify-around hover:border-[#47F969]">Get App <ArrowDownToLine className="transition ease-in delay-250 " /></button>
+                                <button  className="text-white text-xl font-poppins transition duration-500 bg-transparent p-4 ml-0 mx-4 border-2 border-[#224103] rounded-full w-[200px] h-[65px] flex justify-around hover:text-[#47F969] hover:border-[#47F969]">Learn More <Info className="transition ease-in delay-250 " /> </button>
+                                </div>
                             </div>
                         </div>
-                        <div className="mt-20 pr-20 mr-20 z-20" >
-                            <Image src="/Phone.png" alt="phone" width={350} height={100} ></Image>
+                        <div className="flext justify-items-center items-center z-20" >
+                            <Image src="/Phone.png" alt="phone" width={350} height={100} style={{height:"75%", width:"auto",marginInline:"auto",marginTop:"2%"}} ></Image>
                         </div>
                     </div>
                 </div>
